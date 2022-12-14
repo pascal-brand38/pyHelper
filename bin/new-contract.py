83af6e1708c89c65a2796dda9974a23d87e49960
Pascal Brand <pascal.brand38@gmail.com>
2022-12-14 20:28:43 +0100

    bin/new-contract.py: from an excel macros
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- bin/new-contract.py
+++ bin/new-contract.py
@@ -10,6 +10,8 @@ default is
 %1 is the full filename of the old contract we right-click on
 %w is the directory  
 
+# TODO: does not work with accent
+#       requires htmlencode
 
 mkdir -p dev/pascal-brand38
 cd dev/pascal-brand38
@@ -26,66 +28,160 @@ import re
 import os
 import getopt
 from pyHelper import pdfReaderHelper, pdfWriterHelper
+import subprocess
+import html
+import datetime
 
 def usage():
     print('*** ERREUR ***')
     print('new-contract.py [--blank-contract=<>] [--dir-contract=<>] [--old-contract=]')
-    raise Exception('ERREUR DE CONFIGURATION - APPELER PASCAL')
+
+def replace_backslash(arg):
+    return arg.replace('\\', '').replace('C:','XXX')
+
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
+
+def get_immediate_files(a_dir, start):
+    return [name for name in os.listdir(a_dir)
+            if (os.path.isfile(os.path.join(a_dir, name)) and (name.startswith(start))) ]
+
+def unescape(s):
+    try:
+        res = html.unescape(s)
+    except:
+        res = s
+    return res
 
 def get_args(argv):
-    # for a in argv:
-    #     print(a)
-
+    print(argv)
+    
     try:
-        opts, args = getopt.getopt(argv,"h:",["blank-contract=", "dir-contract=", "old-contract="])
-    except getopt.GetoptError:
+        opts, args = getopt.getopt(argv,"h:", [
+            "blank-contract=",
+            "dir-contract=",
+            "old-contract=",
+            "root-contract=",
+            "who=",
+            "from=",
+            "to=",
+            "priceday=",
+            "nbdays=",
+            "services=",
+            "total=",
+            "accompte=",
+            "date_accompte=",
+            "solde=",
+        ])
+    except getopt.GetoptError as e:
         usage()
+        raise e
+
+
+    get_args._blank_contract = ''
+    get_args._dir_contract = ''
+    get_args._old_contract = ''
+    get_args._root_contract = ''
+    get_args._who = ''
+    get_args._from = ''
+    get_args._to= ''
+    get_args._priceday = ''
+    get_args._nb_days = ''
+    get_args._services = '0€'
+    get_args._total = ''
+    get_args._accompte = '0'
+    get_args._date_accompte = ''
+    get_args._solde = ''
 
     # get arguments
     for opt, arg in opts:
         if opt == '-h':
             usage()
         elif opt in ("--blank-contract"):
-            get_args._blank_contract = arg
-            # print('get_args._blank_contract = ' + get_args._blank_contract)
+            get_args._blank_contract = unescape(arg)
         elif opt in ("--dir-contract"):
-            get_args._dir_contract = arg
-            # print('get_args._dir_contract = ' + get_args._dir_contract)
+            get_args._dir_contract = unescape(arg)
         elif opt in ("--old-contract"):
-            get_args._old_contract = arg
-            # print('get_args._old_contract = ' + get_args._old_contract)
+            get_args._old_contract = unescape(arg)
+        elif opt in ("--root-contract"):
+            get_args._root_contract = unescape(arg)
+        elif opt in ("--who"):
+            get_args._who = unescape(arg)
+        elif opt in ("--from"):
+            get_args._from = unescape(arg)
+        elif opt in ("--to"):
+            get_args._to= unescape(arg)
+        elif opt in ("--priceday"):
+            get_args._priceday = unescape(arg)
+        elif opt in ("--nbdays"):
+            get_args._nb_days = unescape(arg)
+        elif opt in ("--services"):
+            get_args._services = unescape(arg)
+        elif opt in ("--total"):
+            get_args._total = unescape(arg)
+        elif opt in ("--accompte"):
+            get_args._accompte = unescape(arg)
+        elif opt in ("--date_accompte"):
+            get_args._date_accompte = unescape(arg)
+        elif opt in ("--solde"):
+            get_args._solde = unescape(arg)
 
-    get_args._new_contract = get_args._dir_contract + '\\..\\' + get_args._blank_contract
+    if get_args._dir_contract != "":
+        get_args._root_contract = get_args._dir_contract + '\\..\\'
+    if get_args._dir_contract == "":
+        # check for the contract dir given get_args._who
+        cat_name = re.sub(' /.*', '', get_args._who)
+        candidates = []
+        subdirs = get_immediate_subdirectories(get_args._root_contract)
+        for subdir in subdirs:
+            if re.sub(' .*', '', subdir) == cat_name:
+                candidates = candidates + [ subdir ]
+        if len(candidates) == 1:
+            get_args._dir_contract = get_args._root_contract + '\\' + candidates[0]
+            print("Répertoire du contrat: ", get_args._dir_contract)
+    
+    if get_args._dir_contract == "":
+            raise Exception('ERREUR: Impossible de déduire le nom du répertoire du contrat')
+
+    if get_args._old_contract == "":
+        files = get_immediate_files(get_args._dir_contract, '20')
+        get_args._old_contract = get_args._dir_contract + '\\' + files[len(files)-1]
     
 def main(argv):
     get_args(argv)
-
     # read old contract
     old_pdf = pdfReaderHelper(open(get_args._old_contract, "rb"))
     dict_old_pdf = old_pdf.get_form_fields_in_list(['/Tx', '/Btn']) # returns a python dictionary
 
-    # clear dictionnary with old booking dates and prices...
+    # replace new dates,...
+    dict_old_pdf['Date darrivée'] = get_args._from
+    dict_old_pdf['Date de départ'] = get_args._to
+    dict_old_pdf['Nombre de jours'] = get_args._nb_days
+    dict_old_pdf['Tarif Journalier'] = get_args._priceday + '€'
+    dict_old_pdf['Total du séjour avec services'] = get_args._total + '€'
+    dict_old_pdf['Acompte de 30  à la réservation'] = get_args._accompte + '€'
+    dict_old_pdf['versé le'] = get_args._date_accompte
+    dict_old_pdf['Le solde de la pension sera versé le jour de larrivée soit'] = get_args._solde + '€'
+    dict_old_pdf['Services soins santé arrivéedépart dimanche'] = get_args._services
+
+    # clear dictionnary
     key_to_clear = [
-        'Date darrivée',
-        'Heure',
-        'Date de départ',
-        'Heure_2',
-        'Nombre de jours',
-        'Tarif Journalier',
-        'Services soins santé arrivéedépart dimanche',
-        'Total du séjour avec services',
-        'Acompte de 30  à la réservation',
-        'versé le',
-        'Le solde de la pension sera versé le jour de larrivée soit'
+        'Heure', 'Heure_2',   # arrival and departure time
+        'Date', 'Nom', 'Date_2', 'Nom_2',       # Puce / vermifuge
     ]
-    for key in key_to_clear:
-        dict_old_pdf.pop(key)
+
+    try:
+        for key in key_to_clear:
+            dict_old_pdf.pop(key)
+    except:
+        pass
 
     # for key in dict_old_pdf:
     #     print(key, '   -----   ', dict_old_pdf[key])
 
     # read new contract
-    new_pdf = pdfReaderHelper(open(get_args._new_contract, "rb"))
+    new_pdf = pdfReaderHelper(open(get_args._root_contract + '\\' + get_args._blank_contract, "rb"))
 
     # write new contract at correct place
     output = pdfWriterHelper()
@@ -98,9 +194,21 @@ def main(argv):
     output.update_page_form_field_values(output.pages[0], dict_old_pdf)
     output.update_page_form_field_values(output.pages[1], dict_old_pdf)
     basename = os.path.basename(get_args._old_contract)
-    new_filename = re.sub('[0-9]+ - [0-9]+', 'nouveau', basename)
+    if get_args._from == '':
+        prefix = 'nouveau - '
+    else:
+        d = datetime.datetime.strptime(get_args._from, '%d/%m/%Y')
+        prefix = datetime.datetime.strftime(d, '%Y - %m - %d') + ' - '
+
+    #new_filename = re.sub('[0-9]{4} - [0-9]{2}', prefix, basename)  # TODO: may have the day!
+    new_filename = basename
+    new_filename = re.sub('^[0-9]*[a-z]? - ', '', new_filename)
+    new_filename = re.sub('^[0-9]*[a-z]? - ', '', new_filename)
+    new_filename = re.sub('^[0-9]*[a-z]? - ', '', new_filename)
     if new_filename == basename:
         new_filename = 'nouveau - ' + get_args._blank_contract
+    else:
+        new_filename = prefix + new_filename
     new_filename = get_args._dir_contract + '/' + new_filename
 
     if os.path.exists(new_filename):
@@ -109,6 +217,14 @@ def main(argv):
     output.write(outputStream)
     outputStream.close()
     print('Le contrat ' + new_filename + ' a été crée')
+    subprocess.Popen([
+        'explorer',
+        get_args._dir_contract
+        ])
+    subprocess.Popen([      # Popen does not wait - call waits
+        'C:\Program Files\Tracker Software\PDF Viewer\PDFXCview.exe',
+        new_filename
+        ])
 
 
 if __name__ == "__main__":
@@ -119,8 +235,9 @@ if __name__ == "__main__":
         print('******** CONTRAT CREE ********')
         print('******************************')
         print('')
-        input('Appuyer sur "Entrée" pour terminer')
+
     except Exception as e:
+        print('++++++++ Exceptions')
         print(e)
         print('')
         print('*********************************')
